70bdcd8917c4fea3e646a06cf47d1c5d0a66d578
Michael Fiedler <michael.fiedler1604@gmail.com>
2024-05-16 16:24:32 +0200

    small change

--- src/Scroll.jsx
+++ src/Scroll.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 const Scroll = (props) => {
   return (
     <div style={{overflowY: 'scroll', border: '2px solid black', height: '700px', width: '100%'}} className='flex flex-wrap justify-center'>
